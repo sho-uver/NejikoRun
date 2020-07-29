a02e8bb68d299f5a7cd0dc1bd25429c291fdecce
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-07-30 01:14:07 +0900

    ステージの自動生成

---
+++ Assets/script/StageGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageGenerator : MonoBehaviour {
+    const int StageChipSize = 30;
+    int currentChipIndex;
+    public Transform character;
+    public GameObject[] stageChips;
+    public int startChipIndex;
+    public int preInstantiate;
+    public List<GameObject> generateStageList = new List<GameObject> ();
+
+    // Start is called before the first frame update
+    void Start () {
+        currentChipIndex = startChipIndex - 1;
+        UpdateStage (preInstantiate);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        int charaPositionIndex = (int) (character.position.z / StageChipSize);
+        if (charaPositionIndex + preInstantiate > currentChipIndex) {
+            UpdateStage (charaPosition)
+        }
+    }
+}
