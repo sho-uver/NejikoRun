8f4d7a7138552425b2567e92f11d971c64740e33
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-07-29 23:57:56 +0900

    カメラの追従 ねじこの自動走行化

--- Assets/NejikoController.cs
+++ Assets/NejikoController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NejikoController : MonoBehaviour {
+    const int MinLane = -2;
+    const int MaxLane = 2;
+    const float LaneWidth = 1.0f;
+
     CharacterController controller;
     Animator animator;
 
     Vector3 moveDirection = Vector3.zero;
+    int targetLane;
+
     public float gravity;
     public float speedZ;
+    public float speedX;
     public float speedJump;
+    public float accelerationZ;
 
     // Start is called before the first frame update
     void Start () {
@@ -19,18 +27,20 @@ public class NejikoController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        if (controller.isGrounded) {
-            if (Input.GetAxis ("Vertical") > 0.0f) {
-                moveDirection.z = Input.GetAxis ("Vertical") * speedZ;
-            } else {
-                moveDirection.z = 0;
-            }
-            transform.Rotate (0, Input.GetAxis ("Horizontal") * 3, 0);
-            if (Input.GetButton ("Jump")) {
-                moveDirection.y = speedJump;
-                animator.SetTrigger ("jump");
-            }
+        if (Input.GetKeyDown ("left")) {
+            MoveToLeft ();
         }
+        if (Input.GetKeyDown ("right")) {
+            MoveToRight ();
+        }
+        if (Input.GetKeyDown ("space")) {
+            Jump ();
+        }
+        float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+        moveDirection.z = Mathf.Clamp (acceleratedZ, 0, speedZ);
+        float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
+        moveDirection.x = ratioX * speedX;
+
         moveDirection.y -= gravity * Time.deltaTime;
         Vector3 globalDirection = transform.TransformDirection (moveDirection);
         controller.Move (globalDirection * Time.deltaTime);
@@ -38,4 +48,21 @@ public class NejikoController : MonoBehaviour {
         animator.SetBool ("run", moveDirection.z > 0.0f);
 
     }
+    public void MoveToLeft () {
+        if (controller.isGrounded && targetLane > MinLane) {
+            targetLane--;
+        }
+    }
+    public void MoveToRight () {
+        if (controller.isGrounded && targetLane < MaxLane) {
+            targetLane++;
+        }
+    }
+    public void Jump () {
+        if (controller.isGrounded) {
+            moveDirection.y = speedJump;
+            animator.SetTrigger ("jump");
+        }
+    }
+
 }
