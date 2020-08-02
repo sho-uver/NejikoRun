772b248e2f5f57f767aaf8f5e0a4aceafd703264
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-08-03 03:12:33 +0900

    ねじこの被ダメージ処理 LIFEUI

--- Assets/script/NejikoController.cs
+++ Assets/script/NejikoController.cs
@@ -6,12 +6,16 @@ public class NejikoController : MonoBehaviour {
     const int MinLane = -2;
     const int MaxLane = 2;
     const float LaneWidth = 1.0f;
+    const int DefaultLife = 3;
+    const float StunDuration = 0.5f;
 
     CharacterController controller;
     Animator animator;
 
     Vector3 moveDirection = Vector3.zero;
     int targetLane;
+    int life = DefaultLife;
+    float recoverTime = 0.0f;
 
     public float gravity;
     public float speedZ;
@@ -19,6 +23,13 @@ public class NejikoController : MonoBehaviour {
     public float speedJump;
     public float accelerationZ;
 
+    public int Life () {
+        return life;
+    }
+
+    bool IsStun () {
+        return recoverTime > 0.0f || life <= 0;
+    }
     // Start is called before the first frame update
     void Start () {
         controller = GetComponent<CharacterController> ();
@@ -36,10 +47,16 @@ public class NejikoController : MonoBehaviour {
         if (Input.GetKeyDown ("space")) {
             Jump ();
         }
-        float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
-        moveDirection.z = Mathf.Clamp (acceleratedZ, 0, speedZ);
-        float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
-        moveDirection.x = ratioX * speedX;
+        if (IsStun ()) {
+            moveDirection.x = 0.0f;
+            moveDirection.z = 0.0f;
+            recoverTime -= Time.deltaTime;
+        } else {
+            float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+            moveDirection.z = Mathf.Clamp (acceleratedZ, 0, speedZ);
+            float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
+            moveDirection.x = ratioX * speedX;
+        }
 
         moveDirection.y -= gravity * Time.deltaTime;
         Vector3 globalDirection = transform.TransformDirection (moveDirection);
@@ -49,19 +66,41 @@ public class NejikoController : MonoBehaviour {
 
     }
     public void MoveToLeft () {
+        if (IsStun ()) {
+            return;
+        }
         if (controller.isGrounded && targetLane > MinLane) {
             targetLane--;
         }
     }
+
     public void MoveToRight () {
+        if (IsStun ()) {
+            return;
+        }
         if (controller.isGrounded && targetLane < MaxLane) {
             targetLane++;
         }
     }
     public void Jump () {
+        if (IsStun ()) {
+            return;
+        }
         if (controller.isGrounded) {
             moveDirection.y = speedJump;
             animator.SetTrigger ("jump");
+        }
+    }
+
+    void OnControllerColliderHit (ControllerColliderHit hit) {
+        if (IsStun ()) {
+            return;
+        }
+        if (hit.gameObject.tag == "Robo") {
+            life--;
+            recoverTime = StunDuration;
+            animator.SetTrigger ("damage");
+            Destroy (hit.gameObject);
         }
     }
 
