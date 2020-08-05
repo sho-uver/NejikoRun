01b8fb14bca3fed93acc7a3c96150a5871205f2f
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-08-05 21:09:40 +0900

    完成

--- Assets/script/GameController.cs
+++ Assets/script/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
@@ -13,8 +14,18 @@ public class GameController : MonoBehaviour {
         int score = CalcScore ();
         scoreText.text = "Score:" + score + "m";
         lifePanel.UpdateLife (nejiko.Life ());
+        if (nejiko.Life () <= 0) {
+            enabled = false;
+            if (PlayerPrefs.GetInt ("HighScore") < score) {
+                PlayerPrefs.SetInt ("HighScore", score);
+            }
+            Invoke ("ReturnToTitle", 2.0f);
+        }
     }
     int CalcScore () {
         return (int) nejiko.transform.position.z;
+    }
+    void ReturnToTitle () {
+        SceneManager.LoadScene ("Title");
     }
 }
---
+++ Assets/script/TitleController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour {
+    public Text highScoreText;
+    public void Start () {
+        highScoreText.text = "High Score:" + PlayerPrefs.GetInt ("HighScore") + "m";
+    }
+    public void OnStartButtonClicked () {
+        SceneManager.LoadScene ("main");
+    }
+}
