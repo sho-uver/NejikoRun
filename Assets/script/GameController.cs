576a46be402633bbdca8edc01da939c08852038b
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-08-05 14:23:23 +0900

    UIの更新

---
+++ Assets/script/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+    public NejikoController nejiko;
+    public Text scoreText;
+    public LifePanel lifePanel;
+
+    // Update is called once per frame
+    public void Update () {
+        int score = CalcScore ();
+        scoreText.text = "Score:" + score + "m";
+        lifePanel.UpdateLife (nejiko.Life ());
+    }
+    int CalcScore () {
+        return (int) nejiko.transform.position.z;
+    }
+}
